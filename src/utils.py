701a6d5eb17ccaaf5169bd26b53ecc6f663b14a2
aaratimgr-png <aaratimgr090@gmail.com>
2026-01-23 14:26:35 +0545

    build api for frontend interfcae

--- src/utils.py
+++ src/utils.py
@@ -51,7 +51,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     
 def load_object(file_path):
     try:
-        with open(file_path,'rb') as file_obj:
+        with open(file_path,"rb") as file_obj:
             return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
